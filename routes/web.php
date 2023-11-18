4e83565efb96fefc72e907abd4c7b5509a63585e
ZackyPrayoga <zackwell.busines@gmail.com>
2023-11-18 16:03:19 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-Route::middleware('auth', 'cekPetugas')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
